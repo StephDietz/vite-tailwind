79a88a2db5e97e71358870c350331eec1516cdf1
Stephani Dietz <steph@Stephanis-MacBook-Pro.local>
2021-07-01 18:02:12 -0500

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="flex flex-col items-center justify-center">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <h1>{{ msg }}</h1>
+    <button @click="count++">count is: {{ count }}</button>
+    <p class="text-pink-400">Edit <code>components/HelloWorld.vue</code> to test hot module replacement.</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+  data() {
+    return {
+      count: 0
+    }
+  }
+}
+</script>
